7447b25eb1b2133cd7d139bef79762df7ec242d5
tonyleuk <tonyleuk@gmail.com>
2025-06-15 14:46:02 +0100

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV = 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   basePath: isProd ? '/reactthreeletter' : '',
