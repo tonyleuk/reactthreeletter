c496ac82d50adfd2f4d0bb99c6768646bb9010e9
tonyleuk <tonyleuk@gmail.com>
2025-06-15 15:07:37 +0100

    test

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const isProd = process.env.NODE_ENV === 'production';
-
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 const nextConfig = {
-  basePath: isProd ? '/reactthreeletter' : '',
+  basePath: isProd ? '' : '/reactthreeletter',
   output: 'export',
   distDir: 'dist',
   images: {
