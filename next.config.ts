8918fe7faaf90ffd368aa45dc9a4d701cf50fc3c
tonyleuk <tonyleuk@gmail.com>
2025-06-15 14:43:49 +0100

    basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? '/reactthreeletter' : '',
   output: 'export',
   distDir: 'dist',
   images: {
