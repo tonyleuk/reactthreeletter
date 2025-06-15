4d3c38074e2ac9666f83239babdc5a95f187117a
tonyleuk <tonyleuk@gmail.com>
2025-06-15 14:53:15 +0100

    ts to js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV = 'production';
+
+const nextConfig = {
+  basePath: isProd ? '/reactthreeletter' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  }
+  /* trailingSlash: true, */
+};
+
+module.exports = nextConfig;
